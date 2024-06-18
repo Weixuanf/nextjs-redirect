4eee2d75ec522f2b20ebe99181fca85cb4c61706
Weixuan Fu <weixuanf@outlook.com>
2024-06-18 09:25:53 +0800

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://nodecafe.co/:path*",
+        permanent: true, // This indicates a 301 redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
